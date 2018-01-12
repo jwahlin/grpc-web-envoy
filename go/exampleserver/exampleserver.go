53e6c1db4b98115b8d03fa03809002a28b7dcf0c
jamie.wahlin <jamie.wahlin@tcnbroadcasting.com>
2018-01-12 11:40:20 -0700

    First commit

---
+++ go/exampleserver/exampleserver.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"fmt"
+	"strings"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+
+	library "_proto/examplecom/library"
+	"golang.org/x/net/context"
+)
+
+var (
+	enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
+	tlsCertFilePath = flag.String("tls_cert_file", "../misc/localhost.crt", "Path to the CRT/PEM file.")
+	tlsKeyFilePath  = flag.String("tls_key_file", "../misc/localhost.key", "Path to the private key file.")
+)
+
+func main() {
+	//flag.Parse()
+	//
+	//grpcServer := grpc.NewServer()
+	//library.RegisterBookServiceServer(grpcServer, &bookService{})
+	//grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
+	//
+	//lis, err := net.Listen("tcp", ":9091")
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//grpcServer.Serve(lis)
+
+
+	port := flag.Int("port", 9091, "grpc port")
+
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	gs := grpc.NewServer()
+	library.RegisterBookServiceServer(gs, &bookService{})
+
+	log.Printf("starting grpc on :%d\n", *port)
+
+	gs.Serve(lis)
+}
+
+type bookService struct{}
+
+var books = []*library.Book{
+	&library.Book{
+		Isbn:   60929871,
+		Title:  "Brave New World",
+		Author: "Aldous Huxley",
+	},
+	&library.Book{
+		Isbn:   140009728,
+		Title:  "Nineteen Eighty-Four",
+		Author: "George Orwell",
+	},
+	&library.Book{
+		Isbn:   9780140301694,
+		Title:  "Alice's Adventures in Wonderland",
+		Author: "Lewis Carroll",
+	},
+	&library.Book{
+		Isbn:   140008381,
+		Title:  "Animal Farm",
+		Author: "George Orwell",
+	},
+}
+
+func (s *bookService) GetBook(ctx context.Context, bookQuery *library.GetBookRequest) (*library.Book, error) {
+	grpc.SendHeader(ctx, metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-unary"))
+	grpc.SetTrailer(ctx, metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-unary"))
+
+	for _, book := range books {
+		if book.Isbn == bookQuery.Isbn {
+			return book, nil
+		}
+	}
+
+	return nil, grpc.Errorf(codes.NotFound, "Book could not be found")
+}
+
+func (s *bookService) QueryBooks(bookQuery *library.QueryBooksRequest, stream library.BookService_QueryBooksServer) error {
+	stream.SendHeader(metadata.Pairs("Pre-Response-Metadata", "Is-sent-as-headers-stream"))
+	for _, book := range books {
+		if strings.HasPrefix(book.Author, bookQuery.AuthorPrefix) {
+			stream.Send(book)
+		}
+	}
+	stream.SetTrailer(metadata.Pairs("Post-Response-Metadata", "Is-sent-as-trailers-stream"))
+	return nil
+}
